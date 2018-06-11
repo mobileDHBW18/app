fad77958efa94cc5dd52d01c9bb89869f6e07e7c
lengers <leo.engers@online.de>
2018-06-11 18:48:21 +0200

    first app draft

---
+++ app/src/main/java/com/example/cantinr/cantinr/MensaSelectActivity.java
@@ -0,0 +1,13 @@
+package com.example.cantinr.cantinr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MensaSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mensa_select);
+    }
+}
