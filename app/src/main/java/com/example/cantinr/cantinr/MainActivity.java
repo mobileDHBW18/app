22052216ff9e90907b181720d6f0c45a1a0538a5
D064040 <jasmin.weimueller@sap.com>
2018-06-12 20:45:07 +0200

    Photo functionality

--- app/src/main/java/com/example/cantinr/cantinr/MainActivity.java
+++ app/src/main/java/com/example/cantinr/cantinr/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cantinr.cantinr;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,17 +16,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
+import com.wonderkiln.camerakit.CameraView;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, CameraKitEventListener {
 
     private ListView mDrawerList;
     private ArrayAdapter<String> mAdapter;
     Intent intentCity;
     Intent intentMensa;
     Intent intentMain;
+    ImageView image;
+    Button photo;
+    CameraView cameraView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,10 +47,12 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
         setTitle("Mensaria Metropol");
         intentMain = getIntent();
+        image = findViewById(R.id.imageView);
         intentCity = new Intent(this, CitySelectActivity.class);
        intentMensa = new Intent(this, MensaSelectActivity.class);
-
-
+        photo = findViewById(R.id.bt_photo);
+        cameraView = (CameraView) findViewById(R.id.camera);
+        cameraView.addCameraKitListener(this);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -49,7 +65,42 @@ public class MainActivity extends AppCompatActivity
         mDrawerList = (ListView)findViewById(R.id.navList);
         addDrawerItems();
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraView.captureImage();
+            }
+        });
+
+
     }
+
+    @Override
+    protected void onResume() {
+      super.onResume();
+      //  cameraView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        //  cameraView.stop();
+       super.onPause();
+    }
+
+    public void onPhotoClick(View aView){
+       cameraView.start();
+    }
+
+    public void onPhotoEnd(View aView){
+        cameraView.stop();
+    }
+
+    public void photoShot(View aView){
+        cameraView.captureImage();
+    }
+
+
 
     private void addDrawerItems() {
         String[] osArray = { "Android", "iOS", "Windows", "OS X", "Linux", "FreeBSD" };
@@ -107,5 +158,28 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onEvent(CameraKitEvent cameraKitEvent) {
+
+    }
+
+    @Override
+    public void onError(CameraKitError cameraKitError) {
+
+    }
+
+    @Override
+    public void onImage(CameraKitImage picture) {
+        picture.getJpeg();
+        Bitmap pic = picture.getBitmap();
+        Toast.makeText(MainActivity.this, "Here!", Toast.LENGTH_SHORT).show();
+        image.setImageBitmap(pic);
+    }
+
+    @Override
+    public void onVideo(CameraKitVideo cameraKitVideo) {
+
     }
 }
